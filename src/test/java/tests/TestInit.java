b8f24d5dc3c01e75f5f93cdf0ba18726e69e9e8e
znamm9 <v.rudenko106@gmail.com>
2023-03-01 16:44:33 +0200

    selenium grid configuration and parallel execution

--- src/test/java/tests/TestInit.java
+++ src/test/java/tests/TestInit.java
@@ -2,18 +2,26 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class TestInit {
 
-//    public WebDriver driver;
     public ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     @BeforeMethod
     public void before(){
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-        driver.set(new ChromeDriver());
+        ChromeOptions chromeOptions = new ChromeOptions();
+        try {
+            driver.set(new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public WebDriver getDriver(){
@@ -22,6 +30,6 @@ public class TestInit {
 
     @AfterMethod
     public void tearDown(){
-        getDriver().quit();
+        driver.get().quit();
     }
 }
